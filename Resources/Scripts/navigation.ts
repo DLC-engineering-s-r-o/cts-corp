7a957a5d568ad42e6bddf62d0f9b4160093af0f6
Lukáš Chylík <lukas.chylik@riganti.cz>
2022-01-25 14:50:56 +0100

    typography updated

--- Resources/Scripts/navigation.ts
+++ Resources/Scripts/navigation.ts
@@ -1,66 +1,12 @@
-const menuBtn = document.getElementById('nebuBtn')
-const nav = document.querySelector('nav')
-const menu = document.querySelector('.nav__menu')
+const menuBtn = document.getElementById('menuBtn') as HTMLButtonElement
+const menu = document.querySelector('.nav__menu') as HTMLDivElement
 const navItems = document.querySelectorAll('.nav__item')
 const htmlDoc = document.querySelector('body')
-const gallery = document.getElementById('gallery')
-const social = document.querySelector('.social')
-const halfDisplayHeight = window.innerHeight / 2
 
 function toggleOpenMenuClasses(){
     menuBtn.classList.toggle('burger--open')
     menu.classList.toggle('nav__menu--open')
     htmlDoc.classList.toggle('overflow-hidden')
-
-    if (nav.classList.contains('nav--dark')){
-        nav.classList.toggle('nav--menu-open')
-        social.classList.toggle('social--menu-open')
-    }
-}
-
-function toggleNavColors(){
-    if(window.pageYOffset >= gallery.offsetTop){
-        nav.classList.add('nav--dark')
-    }
-    else{ 
-        nav.classList.remove('nav--dark')
-    }
-}
-
-function toggleSocialColors(){
-    if(window.pageYOffset >= gallery.offsetTop - halfDisplayHeight){
-        social.classList.add('social--dark')
-    }    
-    else{ 
-        social.classList.remove('social--dark')
-    }
-
-    if (window.pageYOffset >= gallery.offsetTop + halfDisplayHeight){
-        social.classList.remove('social--dark')
-    }
-}
-
-function toggleNavClassesOnScroll(){
-    let lastKnownScrollPosition = 0;
-    let ticking = false;
-
-    function toggleNavClasses(scrollPosition){
-        toggleNavColors()
-        toggleSocialColors()
-    }
-    
-    document.addEventListener('scroll', (e)=>{
-        lastKnownScrollPosition = window.scrollY;
-
-        if (!ticking) {
-            window.requestAnimationFrame(function() {
-                toggleNavClasses(lastKnownScrollPosition);
-              ticking = false;
-            });
-        
-            ticking = true;
-          }
-      })
 }
 
 export function initNav(){
@@ -73,6 +19,4 @@ export function initNav(){
             toggleOpenMenuClasses()
         })
     })
-
-    toggleNavClassesOnScroll()
 }
--- wwwroot/js/init.js
+++ wwwroot/js/init.js
@@ -11,59 +11,14 @@
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.initNav = void 0;
-const menuBtn = document.getElementById('nebuBtn');
-const nav = document.querySelector('nav');
+const menuBtn = document.getElementById('menuBtn');
 const menu = document.querySelector('.nav__menu');
 const navItems = document.querySelectorAll('.nav__item');
 const htmlDoc = document.querySelector('body');
-const gallery = document.getElementById('gallery');
-const social = document.querySelector('.social');
-const halfDisplayHeight = window.innerHeight / 2;
 function toggleOpenMenuClasses() {
     menuBtn.classList.toggle('burger--open');
     menu.classList.toggle('nav__menu--open');
     htmlDoc.classList.toggle('overflow-hidden');
-    if (nav.classList.contains('nav--dark')) {
-        nav.classList.toggle('nav--menu-open');
-        social.classList.toggle('social--menu-open');
-    }
-}
-function toggleNavColors() {
-    if (window.pageYOffset >= gallery.offsetTop) {
-        nav.classList.add('nav--dark');
-    }
-    else {
-        nav.classList.remove('nav--dark');
-    }
-}
-function toggleSocialColors() {
-    if (window.pageYOffset >= gallery.offsetTop - halfDisplayHeight) {
-        social.classList.add('social--dark');
-    }
-    else {
-        social.classList.remove('social--dark');
-    }
-    if (window.pageYOffset >= gallery.offsetTop + halfDisplayHeight) {
-        social.classList.remove('social--dark');
-    }
-}
-function toggleNavClassesOnScroll() {
-    let lastKnownScrollPosition = 0;
-    let ticking = false;
-    function toggleNavClasses(scrollPosition) {
-        toggleNavColors();
-        toggleSocialColors();
-    }
-    document.addEventListener('scroll', (e) => {
-        lastKnownScrollPosition = window.scrollY;
-        if (!ticking) {
-            window.requestAnimationFrame(function () {
-                toggleNavClasses(lastKnownScrollPosition);
-                ticking = false;
-            });
-            ticking = true;
-        }
-    });
 }
 function initNav() {
     menuBtn.addEventListener('click', () => {
@@ -74,7 +29,6 @@ function initNav() {
             toggleOpenMenuClasses();
         });
     });
-    toggleNavClassesOnScroll();
 }
 exports.initNav = initNav;
 
