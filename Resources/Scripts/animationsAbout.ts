6b313ff3a1e100fb9e2efb7d43b1967302821e39
Martin Havelka <martin.havelka@riganti.cz>
2022-02-18 15:24:12 +0100

    counter number

--- Resources/Scripts/animationsAbout.ts
+++ Resources/Scripts/animationsAbout.ts
@@ -10,11 +10,33 @@ function addClassToAboutCards(){
     });
 }
 
+const counterAnim = (qSelector, start = 0, end, duration = 1000) => {
+    const target = document.querySelector(qSelector);
+    let startTimestamp = null;
+
+    const step = (timestamp) => {
+     if (!startTimestamp) startTimestamp = timestamp;
+     const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+     target.innerText = Math.floor(progress * (end - start) + start);
+
+     if (progress < 1) {
+      window.requestAnimationFrame(step);
+     }
+    };
+    
+    window.requestAnimationFrame(step);
+   };
+
 document.addEventListener('scroll', () => {
     scrollPosition = window.scrollY;
     
      if (scrollPosition >= header.offsetTop + (window.innerHeight / 2)) {
         addClassToAboutCards()
      }
+
+    counterAnim("#number-years", 1, 20, 1500);
+    counterAnim("#number-projects", 100, 1000, 1500);
+    counterAnim("#number-money", 1, 3, 1500);  
   });
 }
+
--- wwwroot/js/init.js
+++ wwwroot/js/init.js
@@ -3293,11 +3293,28 @@ function initAboutBox() {
             card.classList.add('about__card--visible');
         });
     }
+    const counterAnim = (qSelector, start = 0, end, duration = 1000) => {
+        const target = document.querySelector(qSelector);
+        let startTimestamp = null;
+        const step = (timestamp) => {
+            if (!startTimestamp)
+                startTimestamp = timestamp;
+            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+            target.innerText = Math.floor(progress * (end - start) + start);
+            if (progress < 1) {
+                window.requestAnimationFrame(step);
+            }
+        };
+        window.requestAnimationFrame(step);
+    };
     document.addEventListener('scroll', () => {
         scrollPosition = window.scrollY;
         if (scrollPosition >= header.offsetTop + (window.innerHeight / 2)) {
             addClassToAboutCards();
         }
+        counterAnim("#number-years", 1, 20, 1500);
+        counterAnim("#number-projects", 100, 1000, 1500);
+        counterAnim("#number-money", 1, 3, 1500);
     });
 }
 exports.initAboutBox = initAboutBox;
