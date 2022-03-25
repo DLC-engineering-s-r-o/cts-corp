6e36d6a0a2c121256c81d9b75c53f7fe4b205909
Lukáš Chylík <lukas.chylik@riganti.cz>
2022-03-25 07:23:19 +0100

    email fixed

--- email.php
+++ email.php
@@ -1,84 +1,54 @@
-<?php
-if(isset($_POST['email'])) {
-	$headers = "Content-Type: text/html; charset=UTF-8";
-    $email_to = "chylik.lukas@gmail.com";
-    $email_subject = "Nová zpráva z webu CTS CORP., s.r.o.";
+<?php 
+$errors = '';
+$myemail = 'chylik.lukas@gmail.com';
+if(empty($_POST['fname'])  || 
+   empty($_POST['lname'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
 
-    function died($error) {
-        // your error code can go here
-        echo "Je nám líto, ale u formuláře, který jste vyplnili, se vyskytla chyba.";
-        echo "Podrobnosti můžete vidět níže.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Vráťte se a opravte tyto chyby.<br /><br />";
-        die();
-    }
+$fname = $_POST['fname']; 
+$lname = $_POST['lname']; 
+$email_address = $_POST['email']; 
+$phone = $_POST['phone']; 
+$message = $_POST['message']; 
 
-    // validation expected data exists
-    if(!isset($_POST['first_name']) ||
-        !isset($_POST['last_name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['telephone']) ||
-        !isset($_POST['comments'])) {
-        died('Je nám líto, při odesílání formuláře se vyskytla chyba.');
-    }
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Neplatná emailová adresa.";
+}
 
-    $first_name = $_POST['first_name']; // required
-    $last_name = $_POST['last_name']; // required
-    $email_from = $_POST['email']; // required
-    $telephone = $_POST['telephone']; // not required
-    $comments = $_POST['comments']; // required
-
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'Zadaná e-mailová adresa je neplatná.<br />';
-  }
-
-  if(strlen($comments) < 10) {
-    $error_message .= 'Zpráva, kterou jste zadali, je neplatná. Zpráva musí obsahovat minimálně 10 znaků.<br />';
-  }
-
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
-
-    $email_message = "Podrobnosti můžete vidět níže.\n\n";
-
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
-
-    $email_message .= "Jméno: ".clean_string($first_name)."\n";
-    $email_message .= "Příjmení: ".clean_string($last_name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Tel: ".clean_string($telephone)."\n";
-    $email_message .= "Zpráva: ".clean_string($comments)."\n";
-
-// create email headers
-$headers = "Content-Type: text/html; charset=UTF-8".'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-mail($email_to, $email_subject, $email_message, $headers);
-}?>
-
-<!DOCTYPE html>
-<html lang="cs-CZ">
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Odeslání kontaktního formuláře: $fname $lname";
+	$email_body = "Nová zpráva z ctscorp.cz. ".
+	" Zde jsou podrobnosti:\n Name: $fname $lname \n Email: $email_address \n Zpráva \n $message"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contact-form-thank-you.html');
+} 
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
 <head>
-    <meta charset="utf-8" />
-    <title>CTS Corp e-mail</title>
-
-    <link rel="stylesheet" href="wwwroot/css/style.min.css" type="text/css" />
+	<title>Contact form handler</title>
 </head>
+
 <body>
-    <section class="sec">
-        <div class="container text-to-center">
-            <h2 class="title">Děkujeme za vaši zprávu</h2>
-             <p class="desc">Zpráva byla úspěšně odeslána. Ozveme se vám v co najkratším možném čase.</p>
-                <a href="index.html" class="button">
-                    <span>Domovská stránka</span>
-                </a>
-        </div>
-    </section>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
 </body>
+</html>
