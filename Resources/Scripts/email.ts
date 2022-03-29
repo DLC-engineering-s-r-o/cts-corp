99f59cdc4b0c815055013f5e2bd919b4f043ae53
Lukáš Chylík <lukas.chylik@riganti.cz>
2022-03-29 10:09:11 +0200

    mail fnc updated

--- Resources/Scripts/email.ts
+++ Resources/Scripts/email.ts
@@ -1,27 +1,28 @@
 export function initEmail() {
-    const fNameEl = document.getElementById('fName') as HTMLInputElement
-    const lNameEl = document.getElementById('lName') as HTMLInputElement
+    const fullNameEl = document.getElementById('fullName') as HTMLInputElement
     const phoneEl = document.getElementById('phone') as HTMLInputElement
     const mailEl = document.getElementById('email') as HTMLInputElement
     const messageEl = document.getElementById('msg') as HTMLTextAreaElement
     const submitBtn = document.getElementById('submitBtn') as HTMLButtonElement
 
-    let fName = ''
-    let lName = ''
+    let fullName = ''
     let email = ''
     let phone = ''
     let message = ''
+    let website = 'ctscorp.cz'
+
+    function httpGet(theUrl) {
+        var xmlHttp = new XMLHttpRequest()
+        xmlHttp.open( "GET", theUrl, false ) // false for synchronous request
+        xmlHttp.send( null )
+        return xmlHttp.responseText
+    }
 
     function setEmailContent() {
 
-        if (fNameEl.value) {
-            fName = fNameEl.value
+        if (fullNameEl.value) {
+            fullName = fullNameEl.value
         }
-
-        if (lNameEl.value) {
-            lName = lNameEl.value
-        }
-
         if (phoneEl.value) {
             phone = phoneEl.value
         }
@@ -34,14 +35,15 @@ export function initEmail() {
             message = messageEl.value
         }
 
-        return fName + lName + phone + email + message
+        return fullName + phone + email + message
     }
 
     submitBtn.addEventListener('click', () => {
         setEmailContent()
 
-        if (fName != '' && lName != '' && email != '' && message != '') {
-            console.log(fName + ' ' + lName + ' ' + email + ' ' + phone + ' ' + message)
+        if (fullName != '' && email != '' && message != '') {
+            httpGet(`https://dlcafsendgrid20220328153355.azurewebsites.net/api/SendEmail?sender=${fullName}&body=${message}`)
+            console.log(`https://dlcafsendgrid20220328153355.azurewebsites.net/api/SendEmail?sender=${fullName}&body=${message}`)
         }
 
         else console.log('%cPlease fill in the required fields!', 'color:red;')
